666062a7d741bf2477543e76bfc57a325b56b2db
vishnu <vishnu@gmail.com>
2023-08-17 12:32:46 +0530

    # Angular Tutorial - 12 - ngIf Directive

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: []
+})
+export class TestComponent {
+
+  displayName= false;
+  constructor() { }
+  
+}
